789422bc77ede6b466a1e4820c0b0e46eacd843c
cat9563 <crysatl9563@gmail.com>
2018-12-15 11:21:27 -0500

    removed models and admin.py to start turtorial

--- core/admin.py
+++ core/admin.py
@@ -1,31 +1,31 @@
 from django.contrib import admin
-from core.models import User, Follow, Blog, Author,BlogNote
+# from core.models import User, Follow, Blog, Author, BlogNote
 
-class BlogNoteInline(admin.StackedInline):
-    model = BlogNote
-    fields = ("body", "description")
-    extra = 1
+# class BlogNoteInline(admin.StackedInline):
+#     model = BlogNote
+#     fields = ("body", "description")
+#     extra = 1
 
-class BlogAdmin(admin.ModelAdmin):
-    list_display = ["title", "author_string"]
-    inlines = [BlogNoteInline]
-    autocomplete_fields = ("authors",)
+# class BlogAdmin(admin.ModelAdmin):
+#     list_display = ["title"]
+#     inlines = [BlogNoteInline]
+#     #autocomplete_fields = ("authors",)
 
-class FollowersInline(admin.StackedInline):
-    model = Follow
-    fk_name = "followed_user"
-    fields = ("following_user",)
-    extra = 1
+# class FollowersInline(admin.StackedInline):
+#     model = Follow
+#     fk_name = "followed_user"
+#     fields = ("following_user",)
+#     extra = 1
 
-class UserAdmin(admin.ModelAdmin):
-    fields = ("username", "email", "is_superuser", "is_staff", "is_active")
-    inlines = [FollowersInline]
+# class UserAdmin(admin.ModelAdmin):
+#     fields = ("username", "email", "is_superuser", "is_staff", "is_active")
+#     inlines = [FollowersInline]
 
-class AuthorAdmin(admin.ModelAdmin):
-    search_fields = ("name",)
+# class AuthorAdmin(admin.ModelAdmin):
+#     search_fields = ("name",)
 
 
-# Register your models here.
-admin.site.register(User, UserAdmin)
-admin.site.register(Blog, BlogAdmin)
-admin.site.register(Author, AuthorAdmin)
+# # Register your models here.
+# admin.site.register(User, UserAdmin)
+# admin.site.register(Blog, BlogAdmin)
+# admin.site.register(Author, AuthorAdmin)
--- core/models.py
+++ core/models.py
@@ -1,43 +1,44 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from django.core.validators import MaxValueValidator, MinValueValidator
+#from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 class User(AbstractUser):
-    users_followed = models.ManyToManyField(
-        to="User",
-        through="Follow",
-        through_fields=("following_user", "followed_user"),
-        related_name="followers",
-    )
+    pass
+    # users_followed = models.ManyToManyField(
+    #     to="User",
+    #     through="Follow",
+    #     through_fields=("following_user", "followed_user"),
+    #     related_name="followers",
+    # )
 
-class Follow(models.Model):
-    following_user = models.ForeignKey(
-        to=User, on_delete=models.CASCADE, related_name="follows_from"
-    )
-    followed_user = models.ForeignKey(
-        to=User, on_delete=models.CASCADE, related_name="follows_to"
-    )
-    created_at = models.DateTimeField(auto_now_add=True, null=False)
+# class Follow(models.Model):
+#     following_user = models.ForeignKey(
+#         to=User, on_delete=models.CASCADE, related_name="follows_from"
+#     )
+#     followed_user = models.ForeignKey(
+#         to=User, on_delete=models.CASCADE, related_name="follows_to"
+#     )
+#     created_at = models.DateTimeField(auto_now_add=True, null=False)
 
-class Author(models.Model):
-    name = models.CharField(max_length=255, unique=True)
+# class Author(models.Model):
+#     name = models.CharField(max_length=255, unique=True)
 
-    def __str__(self):
-        return self.name
+#     def __str__(self):
+#         return self.name
 
-class Blog(models.Model):
-    creator = models.ForeignKey(to=User, on_delete=models.CASCADE, related_name="blogs")
-    title = models.TextField(
-        validators=[MinValueValidator(2), MaxValueValidator(280)],
-    )
-    authors = models.ManyToManyField(Author, related_name="blogs")
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+# class Blog(models.Model):
+#     creator = models.ForeignKey(to=User, on_delete=models.CASCADE, related_name="blogs")
+#     title = models.TextField(
+#         validators=[MinValueValidator(2), MaxValueValidator(280)],
+#     )
+#     authors = models.ManyToManyField(Author, related_name="blogs")
+#     created_at = models.DateTimeField(auto_now_add=True)
+#     updated_at = models.DateTimeField(auto_now=True)
 
-class BlogNote(models.Model):
-    blog = models.ForeignKey(to=Blog, on_delete=models.CASCADE, related_name="notes")
-    body = models.TextField()
-    description = models.CharField(max_length=255)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+# class BlogNote(models.Model):
+#     blog = models.ForeignKey(to=Blog, on_delete=models.CASCADE, related_name="notes")
+#     body = models.TextField()
+#     description = models.CharField(max_length=255)
+#     created_at = models.DateTimeField(auto_now_add=True)
+#     updated_at = models.DateTimeField(auto_now=True)
